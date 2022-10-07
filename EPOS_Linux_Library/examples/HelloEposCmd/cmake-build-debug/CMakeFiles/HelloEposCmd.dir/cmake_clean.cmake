febd2bf69734f197a37a01cd3aab5e437ddbdbf6
Shi Miahui <shimiaohui.cn@gmail.com>
2022-10-07 14:13:17 +0900

    update

---
+++ EPOS_Linux_Library/examples/HelloEposCmd/cmake-build-debug/CMakeFiles/HelloEposCmd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloEposCmd.dir/HelloEposCmd.cpp.o"
+  "CMakeFiles/HelloEposCmd.dir/HelloEposCmd.cpp.o.d"
+  "HelloEposCmd"
+  "HelloEposCmd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloEposCmd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
